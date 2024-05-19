575a10ef67d01578ab9bc391c665a65ffeb1b677
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-18 23:17:30 -0500

    Usando router-dom con componets Login-ForgetPassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./views/auth/Login";
+import ForgetPassword from "./views/auth/ForgetPassword";
 function App() {
     return (
-        <>
-            <h1 className="text-3xl font-bold">Haciendola FrontEnd</h1>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Login />} />
+                    <Route
+                        path="forget-password"
+                        element={<ForgetPassword />}
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
