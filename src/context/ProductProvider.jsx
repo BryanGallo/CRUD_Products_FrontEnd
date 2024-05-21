4807d9f05bc5d95aedd7d9adfe1a4a2a88133418
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-20 22:09:15 -0500

    Moviendo useEffect y state a ProductProvider

--- src/context/ProductProvider.jsx
+++ src/context/ProductProvider.jsx
@@ -1,10 +1,37 @@
-import { useState, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
+import clientAxios from "../config/clientAxios";
 
 const ProductContext = createContext();
 
-export default function ProductProvider({ children }) {
+function ProductProvider({ children }) {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                if (!token) {
+                    return;
+                }
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                };
+                const { data } = await clientAxios("/api/product", config);
+                console.log(data);
+                setProducts(data);
+            } catch (error) {
+                console.log(error.response);
+            }
+        };
+        getProducts();
+    }, []);
     return (
-        <ProductContext.Provider value={{}}>{children}</ProductContext.Provider>
+        <ProductContext.Provider value={{ products }}>
+            {children}
+        </ProductContext.Provider>
     );
 }
 export { ProductProvider };
+export default ProductContext;
--- src/views/product/Products.jsx
+++ src/views/product/Products.jsx
@@ -1,33 +1,8 @@
-import { useState, useEffect } from "react";
-import clientAxios from "../../config/clientAxios";
 import Product from "./Product";
+import useProduct from "../../hooks/useProduct";
 export default function Products() {
-    const [products, setProducts] = useState([]);
-
-
-    useEffect(() => {
-        const getProducts = async () => {
-            try {
-                const token = localStorage.getItem("token");
-                if (!token) {
-                    return;
-                }
-                const config = {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    },
-                };
-                const { data } = await clientAxios("/api/product", config);
-                console.log(data);
-                setProducts(data);
-            } catch (error) {
-                console.log(error.response);
-            }
-        };
-        getProducts();
-    }, []);
-
+    const { products } = useProduct();
+    console.log(products.length);
     return (
         <div className="bg-gray-100">
             <div className=" md:min-h-screen">
